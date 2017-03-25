34a4656e1f824c4a695dfdb8254177e06b2e4322
Martin Zemek <xzemek04@stud.fit.vutbr.cz>
2017-03-25 03:35:49 +0100

    fixed parsing grammar

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Parser.CFGParser
 import Parser.OptionsParser
 import System.Environment
 import Type.OptionsParser
+import Type.CFGParser
 
 main :: IO ()
 main
@@ -12,13 +13,16 @@ main
     --read file
   input <- getInput (filepath opts)
     --parse context free grammar
---   grammar <- case parseCFG input of
---       Left e -> do
---         putStrLn "Error parsing input:" 
---         print e
---       Right r -> r
+  case parseCFG input of
+      Left e -> do
+        putStrLn "Error parsing input:" 
+        print e
+      Right r -> runMode (mode opts) r
     --run selected mode
-  case mode opts of
-    PrintMode -> print input
-    SimpleMode -> print "mode SimpleMode not implemented yet"
-    CnfMode -> print "mode CnfMode not implemented yet"
+
+runMode :: Mode -> TCFGrammar -> IO ()
+runMode mode grammar=  
+     case mode of
+        PrintMode -> print grammar
+        SimpleMode -> print "mode SimpleMode not implemented yet"
+        CnfMode -> print "mode CnfMode not implemented yet"
